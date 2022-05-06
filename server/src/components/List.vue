d99b64d1efe41b3e11434745c4530748728867bb
maru0804 <sakakibara.maruto719@mail.kyutech.jp>
2022-05-06 15:42:47 +0900

    feat : change view style

--- server/src/components/List.vue
+++ server/src/components/List.vue
@@ -6,8 +6,14 @@
             <input type="text" v-model="newItem">
             <button v-on:click="addItem">Add</button>
         </form>
-        <pre>{{$data}}</pre>
 
+        <table>
+            <tr v-for="(todo, key) in todos" :key="key">
+                <td><input type="checkbox" v-model="todo.isDone"></td>
+                <td><span v-bind:class="{done: todo.isDone}">{{todo.item}}</span></td>
+                <td><button v-on:click="deleteItem(key)">Delete</button></td>
+            </tr>
+        </table>
     </div>
 </template>
 
@@ -25,11 +31,26 @@ export default {
         return
       }
       var todo = {
-        item: this.newItem
+        item: this.newItem, 
+        isDone: false 
       }
       this.todos.push(todo)
       this.newItem = ''
+    },
+    deleteItem: function (key) {
+      this.todos.splice(key, 1)
     }
   }
 }
 </script>
+
+<style>
+  #app table{
+    margin-left: auto;
+    margin-right: auto;
+    text-align: left;
+  }
+ /*  #app span.done{
+    text-decoration:line-through;
+  } */
+</style>
